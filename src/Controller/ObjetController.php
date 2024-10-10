6cfc25a876b6c993801d97e03a2ac31bfddff77f
melaa <melaa976@gmail.com>
2024-10-10 16:40:17 +0200

    creation des objets

--- src/Controller/ObjetController.php
+++ src/Controller/ObjetController.php
@@ -2,17 +2,41 @@
 
 namespace App\Controller;
 
+use App\Repository\ObjetRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ObjetController extends AbstractController
 {
+
     #[Route('/objet', name: 'app_objet')]
     public function index(): Response
     {
+
         return $this->render('objet/index.html.twig', [
             'controller_name' => 'ObjetController',
+        ]);
+    }
+
+
+    #[Route('/objet', name: 'app_objet')]
+    public function allObjets(ObjetRepository $allObjets): Response
+    {
+        $Objets = $allObjets->findAll();
+        return $this->render('objet/index.html.twig', [
+            'allObjets' => $Objets
+            
+        ]);
+    }
+
+    #[Route('/objet/{id}', name: 'app_objetItem')]
+    public function objetItem(ObjetRepository $objetItem, int $id): Response
+    {
+        $objetItem = $objetItem->find($id);
+        dump($objetItem);
+        return $this->render('objet/objetItem.html.twig',[
+            'objetItem'=> $objetItem,
         ]);
     }
 }
