91c1c6dc94450418664c11c0c36e4c2de9600f3e
melaa <melaa976@gmail.com>
2024-10-15 07:14:40 +0200

    creation des CRUDS

--- src/Controller/ObjetController.php
+++ src/Controller/ObjetController.php
@@ -2,41 +2,80 @@
 
 namespace App\Controller;
 
+use App\Entity\Objet;
+use App\Form\ObjetType;
 use App\Repository\ObjetRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class ObjetController extends AbstractController
+#[Route('/objet')]
+final class ObjetController extends AbstractController
 {
-
-    #[Route('/objet', name: 'app_objet')]
-    public function index(): Response
+    #[Route(name: 'app_objet_index', methods: ['GET'])]
+    public function index(ObjetRepository $objetRepository): Response
     {
-
         return $this->render('objet/index.html.twig', [
-            'controller_name' => 'ObjetController',
+            'objets' => $objetRepository->findAll(),
         ]);
     }
 
-
-    #[Route('/objet', name: 'app_objet')]
-    public function allObjets(ObjetRepository $allObjets): Response
+    #[Route('/new', name: 'app_objet_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
-        $Objets = $allObjets->findAll();
-        return $this->render('objet/index.html.twig', [
-            'allObjets' => $Objets
-            
+        $objet = new Objet();
+        $form = $this->createForm(ObjetType::class, $objet);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($objet);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_objet_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('objet/new.html.twig', [
+            'objet' => $objet,
+            'form' => $form,
         ]);
     }
 
-    #[Route('/objet/{id}', name: 'app_objetItem')]
-    public function objetItem(ObjetRepository $objetItem, int $id): Response
+    #[Route('/{id}', name: 'app_objet_show', methods: ['GET'])]
+    public function show(Objet $objet): Response
     {
-        $objetItem = $objetItem->find($id);
-        dump($objetItem);
-        return $this->render('objet/objetItem.html.twig',[
-            'objetItem'=> $objetItem,
+        return $this->render('objet/show.html.twig', [
+            'objet' => $objet,
         ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_objet_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Objet $objet, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(ObjetType::class, $objet);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_objet_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('objet/edit.html.twig', [
+            'objet' => $objet,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_objet_delete', methods: ['POST'])]
+    public function delete(Request $request, Objet $objet, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$objet->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($objet);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_objet_index', [], Response::HTTP_SEE_OTHER);
     }
 }
